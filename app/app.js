af0343dde14087e6f0b0a935a0ea2a1e448841df
Terry <yifan@yang.com>
2017-08-03 02:19:19 -0400

    Integrate BootStrap template to Angular. Need to implement the details for each page.

--- app/app.js
+++ app/app.js
@@ -14,32 +14,91 @@
         $stateProvider
             .state('home', {
                 url: '/',
-                templateUrl: 'home/index.html',
+                templateUrl: 'components/home/index.html',
                 controller: 'Home.IndexController',
-                controllerAs: 'vm',
-                data: { activeTab: 'home' }
+                controllerAs: 'vm'
             })
-            .state('account', {
-                url: '/account',
-                templateUrl: 'account/index.html',
-                controller: 'Account.IndexController',
-                controllerAs: 'vm',
-                data: { activeTab: 'account' }
+            .state('flot-charts', {
+                url: '/float-charts',
+                templateUrl: 'components/fcharts/index.html',
+                controller: 'Fcharts.IndexController',
+                controllerAs: 'vm'
             })
-            .state('paymentmethod',{
-                url: '/paymentmethod',
-                templateUrl: 'paymentmethod/index.html',
-                controller: 'Paymentmethod.IndexController',
-                controllerAs: 'vm',
-                data: { activeTab: 'paymentmethod' }
+            .state('morris-charts', {
+                url: '/morris-charts',
+                templateUrl: 'components/mcharts/index.html',
+                controller: 'Mcharts.IndexController',
+                controllerAs: 'vm'
             })
-            .state('myasset', {
-                url:'/assets',
-                templateUrl:'assets/index.html',
-                controller: 'Addassets.IndexController',
-                controllerAs: 'vm',
-                data: { activeTab: 'myasset'}
+            .state('tables', {
+                url: '/tables',
+                templateUrl: 'components/tables/index.html',
+                controller: 'Tables.IndexController',
+                controllerAs: 'vm'
+            })
+            .state('forms', {
+                url: '/forms',
+                templateUrl: 'components/forms/index.html',
+                controller: 'Forms.IndexController',
+                controllerAs: 'vm'
+            })
+            .state('panels', {
+                url: '/panels',
+                templateUrl: 'components/panels/index.html',
+                controller: 'Panels.IndexController',
+                controllerAs: 'vm'
+            })
+            .state('buttons', {
+                url: '/buttons',
+                templateUrl: 'components/buttons/index.html',
+                controller: 'Buttons.IndexController',
+                controllerAs: 'vm'
+            })
+            .state('notifications', {
+                url: '/notifications',
+                templateUrl: 'components/notifications/index.html',
+                controller: 'Notifications.IndexController',
+                controllerAs: 'vm'
+            })
+            .state('typography', {
+                url: '/typography',
+                templateUrl: 'components/typography/index.html',
+                controller: 'Typography.IndexController',
+                controllerAs: 'vm'
+            })
+            .state('icons', {
+                url: '/icons',
+                templateUrl: 'components/icons/index.html',
+                controller: 'Icons.IndexController',
+                controllerAs: 'vm'
+            })
+            .state('grid', {
+                url: '/grid',
+                templateUrl: 'components/grid/index.html',
+                controller: 'Grid.IndexController',
+                controllerAs: 'vm'
             });
+            //.state('account', {
+            //    url: '/account',
+            //    templateUrl: 'account/index.html',
+            //    controller: 'Account.IndexController',
+            //    controllerAs: 'vm',
+            //    data: { activeTab: 'account' }
+            //})
+            //.state('paymentmethod',{
+            //    url: '/paymentmethod',
+            //    templateUrl: 'paymentmethod/index.html',
+            //    controller: 'Paymentmethod.IndexController',
+            //    controllerAs: 'vm',
+            //    data: { activeTab: 'paymentmethod' }
+            //})
+            //.state('myasset', {
+            //    url:'/assets',
+            //    templateUrl:'assets/index.html',
+            //    controller: 'Addassets.IndexController',
+            //    controllerAs: 'vm',
+            //    data: { activeTab: 'myasset'}
+            //});
     }
 
     function run($http, $rootScope, $window) {
@@ -54,7 +113,7 @@
             //window.alert("toParams---" + JSON.stringify(toParams));
             //window.alert("fromState---" + JSON.stringify(fromState));
             //window.alert("fromParams---" + JSON.stringify(fromParams));
-            $rootScope.activeTab = toState.data.activeTab;
+            //$rootScope.activeTab = toState.data.activeTab;
         });
     }
 
---
+++ app/components/notifications/index.controller.js
@@ -0,0 +1,22 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('Notifications.IndexController', Controller);
+
+    function Controller(UserService) {
+        var vm = this;
+
+        initController();
+
+        function initController() {
+            //window.alert("home.controller ---");
+            // get current user
+            UserService.GetCurrent().then(function (user) {
+                vm.user = user;
+            });
+        }
+    }
+
+})();
